f9c5c750ba1443d8f1aeaafe9977b76a52e471f8
SaraLindholm <sara.lindholm88@gmail.com>
2024-12-20 15:28:29 +0100

    llyckats få e bild att visas

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,4 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.ctfassets.net",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
